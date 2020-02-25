f27251f44d34f51be73cd05ce9c4a3b703be3a1a
MahesaAlwi <43037291+mahesaalwi@users.noreply.github.com>
2020-02-25 17:41:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,12 @@ $api_url = $is_production ?
 
 
 if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
-  http_response_code(200); 
+  http_response_code(404); 
   echo "wrong path, make sure it's `/charge`"; exit();
 }
 
 if( $_SERVER['REQUEST_METHOD'] !== 'POST'){
-  http_response_code(200);
+  http_response_code(404);
   echo "Page not found or wrong HTTP request method is used"; exit();
 }
 
